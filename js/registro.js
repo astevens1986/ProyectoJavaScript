be38716078d05927fc5e33263e7c212794cce96b
astevens1986 <43353803+astevens1986@users.noreply.github.com>
2020-07-29 22:00:13 -0500

    Update registro.js
    Update registro.js

--- js/registro.js
+++ js/registro.js
@@ -2,6 +2,36 @@
 
 function validar(formulario) {
 
-  //Expresion regular del correo
+  if (formulario.nombres.value.trim().length == 0) {
+    alert("Nombre obligatorio");
+    return false;
+  }
 
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (!re.test(formulario.email.value)) {
+    alert("Email inválido");
+    return false;
+  }
+
+  if (formulario.contrasena.value.trim().length == 0) {
+    alert("Contraseña obligatorio");
+    return false;
+  }
+
+  if (formulario.contrasena.value != formulario.confirmacion.value) {
+    alert("Confirmación no coincide");
+    return false;
+  }
+
+  if (formulario.tipo.value == "-1") {
+    alert("Es obligatorio colocar el tipo");
+    return false;
+  }
+
+  if (!formulario.acepto.checked) {
+    alert("Debe aceptar los términos y condiciones");
+    return false;
+  }
+
+  return false;
 }
